075cbc4fa3da3433302c59ea514f253c1c988fa3
root <root@Msypro.localdomain>
2023-03-17 14:53:09 +0000

    Displays all miniscule alphabet letter

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *  *main - Entry point
+ *   *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 97; i <= 122; i++)
+	{
+		char letter = (char) i;
+
+		putchar(letter);
+		return (0);
+	}
+}
