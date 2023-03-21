3c7b3f6b24b22bfe58cb092ec3478499a1cdb7de
root <root@Msypro.localdomain>
2023-03-21 12:13:35 +0000

    Print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
-#include <main.h>
+#include "main.h"
 /**
  *  *main - Entry point
  *   *Return: Always 0 (Success)
  */
 int main(void)
 {
-	char ref[] = "_putchar";
-
-	printf("%s\n", ref);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
+
