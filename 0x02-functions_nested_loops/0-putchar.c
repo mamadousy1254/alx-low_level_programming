73a347920873346a6d7decabd74d273c465f8d7f
root <root@Msypro.localdomain>
2023-03-21 11:28:45 +0000

    Print string _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *  *main - Entry point
+ *   *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ref[] = "_putchar";
+
+	printf("%s\n", ref);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Start writting functions
