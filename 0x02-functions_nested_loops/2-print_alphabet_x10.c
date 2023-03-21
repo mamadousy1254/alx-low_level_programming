9f481e669f3fc068b95e1389582cbcfe22630d80
root <root@Msypro.localdomain>
2023-03-21 14:17:03 +0000

    Prints alphabet letter 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *  * print_alphabet_x10 - Make the alphabet
+ *   * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+
+	char j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+
+		}
+		_putchar('\n');
+	}
+}
