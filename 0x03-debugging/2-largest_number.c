1b69a5287fb5bcea49c40ed9db174aef5cb06134
root <root@Msypro.localdomain>
2023-03-22 11:01:15 +0000

    The largest number printed

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -6,7 +6,6 @@
  *     * @c: third integer
  *      * Return: largest number
  */
-
 int largest_number(int a, int b, int c)
 {
 	int largest;
@@ -15,11 +14,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (a > b && c > a)
-	{
-		largest = c;
-	}
-	else if (b > c)
+	else if (b > a && b > c)
 	{
 		largest = b;
 	}
@@ -28,5 +23,5 @@ int largest_number(int a, int b, int c)
 		largest = c;
 	}
 	return (largest);
-	}
 }
+
