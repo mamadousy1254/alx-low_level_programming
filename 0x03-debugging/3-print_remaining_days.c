af90f08d8f3b2e55c2979166f33ab4ca54dbdf66
root <root@Msypro.localdomain>
2023-03-22 12:08:59 +0000

    test bisextile year or not

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * * print_remaining_days - takes a date and prints how many days are
- * * left in the year, taking leap years into account
- * * @month: month in number format
- * * @day: day of month
- * * @year: year
- * * Return: void
+ *   * print_remaining_days - takes a date and prints how many days are
+ *     * left in the year, taking leap years into account
+ *       * @month: month in number format
+ *         * @day: day of month
+ *           * @year: year
+ *             * Return: void
  */
 void print_remaining_days(int month, int day, int year)
 {
@@ -27,8 +27,8 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
-		}											}
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 365 - day);
+		}
 	}
 }
