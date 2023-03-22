77a8f9ff51e1087e7ec68419ddf69d41871a376d
root <root@Msypro.localdomain>
2023-03-22 20:19:34 +0000

    Test is number is positive, negative or zero

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ *  * main - prints sign of number
+ *   *
+ *    * Return: 0 if no error
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	return (0);
+}
