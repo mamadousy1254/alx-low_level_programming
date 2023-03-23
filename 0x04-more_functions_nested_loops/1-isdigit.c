a41e07475b52fd00157364b219b2ea1085db25a6
root <root@Msypro.localdomain>
2023-03-23 10:24:39 +0000

    Check if digit or not

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- *  * _isdigit - checks for digit or not
- *   * @c: digit that is checked
- *    * Return: if uppercase returns 0 otherwise
+ *  * _isdigit- checks for a digit 0 through 9
+ *   * @c: input
+ *    * Return: if digit returns 0 otherwise 1
  */
 
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
