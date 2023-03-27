cfeb91de1616aebb2be9a0fe3913e991633dabf1
root <root@Msypro.localdomain>
2023-03-27 14:53:40 +0100

    Intervert two integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * swap_int - swaps the values
- * @a: integer to swap
- * @b: integer to swap
+ *  * swap_int - swaps the values of two integers
+ *   * @a: integer to swap
+ *    * @b: integer to swap
  */
 
 void swap_int(int *a, int *b)
 {
 	int tmp;
 
-	temp = *a;
-
+	tmp = *a;
 	*a = *b;
-
 	*b = tmp;
 }
