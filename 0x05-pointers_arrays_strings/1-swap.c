987b7690bf6c12c36d1bb047b58c5eafe03b874d
root <root@Msypro.localdomain>
2023-03-27 14:47:41 +0100

    Swap values into two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values
+ * @a: integer to swap
+ * @b: integer to swap
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	temp = *a;
+
+	*a = *b;
+
+	*b = tmp;
+}
