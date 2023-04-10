31023ef16251e0e51f90a1166536a324f35a9268
root <root@Msypro.localdomain>
2023-04-10 19:55:18 +0100

    A function multiply two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ ** main - program that multiplies two numbers
+ ** @argc: argument count
+ ** @argv: argument vector
+ ** Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int i, j;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	printf("%d\n", i * j);
+	return (0);
+}
